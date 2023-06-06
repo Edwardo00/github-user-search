dc904d3392964dd5c70b437dc24cd394bd2a5743
Edwardo00 <edydumy@gmail.com>
2023-06-06 21:41:07 +0300

    github-user-search

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_URL = "https://api.github.com"
+
+const fetchResults = async (query) => {
+    try {
+        const response = await fetch(`${API_URL}/search/users?q=${query}`)
+        const json = await response.json()
+        return json.items || []
+    } catch (e) {
+        throw new Error(e)
+    }
 }
 
-export default App;
+const App = () => {
+    const [query, setQuery] = useState('')
+    const [results, setResults] = useState([])
+
+    const onSearchChange = (event) => {
+        setQuery(event.target.value)
+    }
+
+    const onSearchSubmit = async (event) => {
+        event.preventDefault()
+        const results = await fetchResults(query)
+        setResults(results)
+    }
+
+  return (
+    <div>
+        <h1>GitHub User Search</h1>
+        <Form 
+            onChange={onSearchChange}
+            onSubmit={onSearchSubmit}
+            value={query}
+        />
+        {results.map((user) => (
+            <User 
+                key={user.login}
+                avatar={user.avatar_url}
+                url={user.html_url}
+                username={user.login}
+            />
+        ))}
+    </div>
+  )
+}
+
+const User = ({avatar, url, username}) => {
+    return (
+        <div className='user'>
+            <img src={avatar} alt='Profile' width='100' height='100' />
+            <a href={url} target='_blank' rel='noopener noreferrer'>
+                {username}
+            </a>
+        </div>
+    )
+}
+
+const Form = ({onSubmit, onChange, value}) => {
+    return (
+        <form onSubmit={onSubmit}>
+            <input 
+                type='text'
+                placeholder='Enter username'
+                onChange={onChange}
+                value={value}
+            />
+            <button type='submit'>Search</button>
+        </form>
+    )
+}
+
+export default App
